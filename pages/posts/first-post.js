c1852752bbf644da0d214c6dac00d3e824e580ac
unknown <dianwls032604@naver.com>
2024-04-09 12:17:07 +0900

    merge

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>HEllo this is Kim SEo Hyun! I'm major in department of Information System.</h1>
+      <h2>
+        <Link href="/">Back to home~</Link>
+      </h2>
+    </>
+  );
+}
